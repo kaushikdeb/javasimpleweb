1f0a528e202e99f8d766a6b77060566341fc4755
Kaushik Deb <kdeb@secureworks.com>
2019-10-22 12:52:46 -0400

    Fix issue with texts with spaces

--- src/main/java/org/secureworks/pe/javasimpleweb/SimpleServlet.java
+++ src/main/java/org/secureworks/pe/javasimpleweb/SimpleServlet.java
@@ -2,6 +2,7 @@ package org.secureworks.pe.javasimpleweb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,39 +18,43 @@ import org.apache.http.util.EntityUtils;
 
 @SuppressWarnings("serial")
 public class SimpleServlet extends HttpServlet {
+
+        private String TOP_HTML_TEXT = "<!DOCTYPE html>\r\n" +
+                        "<html lang=\"en\">\r\n" +
+                        "<head>\r\n" +
+                        "  <title>PE Java Simple Web</title>\r\n" +
+                        "  <meta charset=\"utf-8\">\r\n" +
+                        "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" +
+                        "  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">\r\n" +
+                        "  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>\r\n" +
+                        "  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>\r\n" +
+                        "</head>\r\n" +
+                        "<body>\r\n" +
+                        "\r\n" +
+                        "<div class=\"container\">" +
+                        "<h2>Response from backend Jersey REST API</h2>";
+
 	
-	private String TOP_HTML_TEXT = "<!DOCTYPE html>\r\n" + 
-			"<html lang=\"en\">\r\n" + 
-			"<head>\r\n" + 
-			"  <title>PE Java Simple Web</title>\r\n" + 
-			"  <meta charset=\"utf-8\">\r\n" + 
-			"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
-			"  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">\r\n" + 
-			"  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>\r\n" + 
-			"  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>\r\n" + 
-			"</head>\r\n" + 
-			"<body>\r\n" + 
-			"\r\n" + 
-			"<div class=\"container\">";
-	
-	private String BOTTOM_HTML_TEXT = "</div>\r\n" + 
-			"<button onclick=\"goBack()\">Go Back</button>" + 
-			"</body>\r\n" + 
-			"</html>";
+    private String BOTTOM_HTML_TEXT =
+            "<button onclick=\"history.back(-1)\">Go Back</button>" +
+            "</div>\r\n" +
+                        "</body>\r\n" +
+                        "</html>";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
-    	
-    	String text = request.getParameter("text");
-    	String hashingAlgorithm = request.getParameter("hashingAlgorithm");
-    	String restURL = "http://jerseysimplerest/myapp/" + hashingAlgorithm + 
-    			"/" + text;
-    	
+
+        String text = request.getParameter("text");
+        text = URLEncoder.encode(text, "UTF-8");
+        String hashingAlgorithm = request.getParameter("hashingAlgorithm");
+        String restURL = "http://localhost:8081/myapp/" + hashingAlgorithm +
+                        "/" + text;
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println(TOP_HTML_TEXT);
-        
+
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(restURL);
         CloseableHttpResponse response1 = httpclient.execute(httpGet);
@@ -66,7 +71,7 @@ public class SimpleServlet extends HttpServlet {
             // do something useful with the response body
             // and ensure it is fully consumed
             out.println(EntityUtils.toString(entity1));
-            
+
             EntityUtils.consume(entity1);
         } finally {
             response1.close();
@@ -74,8 +79,8 @@ public class SimpleServlet extends HttpServlet {
 
 
         out.println(BOTTOM_HTML_TEXT);
-        
+
     }
-    
-    
+
+
 }
